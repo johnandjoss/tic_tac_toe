9e015110b988be59ddbc9872633f11efcfab8862
James Robert Williams & Adam McFadden <student@epicodus.com>
2015-06-03 10:18:28 -0700

    add documentation of constructors and prototypes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
+//Player contructor creates the player with a set mark ("X" or "O") and active state (true or false)
+
 function Player(mark, active) {
   this.mark = mark
   this.active = active
 };
+
+//toggleTurn will switch a player's active state to the opposite of its current
 
 Player.prototype.toggleTurn = function() {
   if (this.active === false) {
@@ -10,6 +14,8 @@ Player.prototype.toggleTurn = function() {
     this.active = false
   }
 };
+
+//Board constructor will create a 2D array with each space being filled with null. It will always create a square and the size is determined by the argument sideLength. 3 is the standard sideLength for tic-tac-toe.
 
 function Board(sideLength) {
   this.sideLength = sideLength;
@@ -23,6 +29,8 @@ function Board(sideLength) {
   this.board = board
 };
 
+//spaces will return the total number of spaces on the board.
+
 Board.prototype.spaces = function() {
   var totalSpaces = null;
   this.board.forEach(function(space) {
@@ -31,11 +39,15 @@ Board.prototype.spaces = function() {
   return totalSpaces;
 };
 
+//mark will mark a (x, y) coord with the passed in character ("X" or "O")
+
 Board.prototype.mark = function (xcoord, ycoord, mark) {
   if (!(this.isMarked(xcoord, ycoord))) {
     this.board[xcoord][ycoord] = mark
   }
 };
+
+//isMarked will return the current mark of a space. If space is not marked it will return false.
 
 Board.prototype.isMarked = function (xcoord, ycoord) {
   if (this.board[xcoord][ycoord] !== null) {

8ad1e439ca72a5048ff14ee0b4646874307464e3
jeanette fairless & jocelyn alsdorf <student@epicodus.com>
2015-06-04 14:01:18 -0700

    fixes bug in AI when AI would choose a square already played

--- js/scripts.js
+++ js/scripts.js
@@ -155,11 +155,13 @@ var yGuess;
           $("#results").slideDown("slow");
           $("#winner").text("Player" + " " + game.whoseTurn().mark + " WINS!");
         }
+
       game.changeTurn()
 
       if (game.whoseTurn().mark === "O") {
 
-      compuTurn();
+        compuTurn();
+        alert("o turn");
       }
 
       $("#players h1").text("Player" + " " + game.whoseTurn().mark + "'s Turn!");
@@ -183,13 +185,14 @@ var yGuess;
           $("#results").slideDown("slow");
           $("#winner").text("Player" + " " + game.whoseTurn().mark + " WINS!");
         }
+
       game.changeTurn()
 
+      if (game.whoseTurn().mark === "O") {
 
-            if (game.whoseTurn().mark === "O") {
-
-            compuTurn();
-            }
+        compuTurn();
+        alert("o turn");
+      }
 
       $("#players h1").text("Player" + " " + game.whoseTurn().mark + "'s Turn!");
       $("body").removeClass();
@@ -211,10 +214,13 @@ var yGuess;
           $("#results").slideDown("slow");
           $("#winner").text("Player" + " " + game.whoseTurn().mark + " WINS!");
         }
+
         game.changeTurn();
+
         if (game.whoseTurn().mark === "O") {
 
-        compuTurn();
+          compuTurn();
+          alert("o turn");
         }
 
         $("#players h1").text("Player" + " " + game.whoseTurn().mark + "'s Turn!");
@@ -238,11 +244,12 @@ var yGuess;
           $("#winner").text("Player" + " " + game.whoseTurn().mark + " WINS!");
         }
 
-
         game.changeTurn();
+
         if (game.whoseTurn().mark === "O") {
 
-        compuTurn();
+          compuTurn();
+          alert("o turn");
         }
 
         $("#players h1").text("Player" + " " + game.whoseTurn().mark + "'s Turn!");
@@ -266,12 +273,12 @@ var yGuess;
           $("#winner").text("Player" + " " + game.whoseTurn().mark + " WINS!");
         }
 
-
-
         game.changeTurn();
+
         if (game.whoseTurn().mark === "O") {
 
-        compuTurn();
+          compuTurn();
+          alert("o turn");
         }
 
         $("#players h1").text("Player" + " " + game.whoseTurn().mark + "'s Turn!");
@@ -295,11 +302,12 @@ var yGuess;
           $("#winner").text("Player" + " " + game.whoseTurn().mark + " WINS!");
         }
 
-
         game.changeTurn();
+
         if (game.whoseTurn().mark === "O") {
 
-        compuTurn();
+          compuTurn();
+          alert("o turn");
         }
 
         $("#players h1").text("Player" + " " + game.whoseTurn().mark + "'s Turn!");
@@ -323,11 +331,12 @@ var yGuess;
           $("#winner").text("Player" + " " + game.whoseTurn().mark + " WINS!");
         }
 
-
         game.changeTurn();
+
         if (game.whoseTurn().mark === "O") {
 
-        compuTurn();
+          compuTurn();
+          alert("o turn");
         }
 
         $("#players h1").text("Player" + " " + game.whoseTurn().mark + "'s Turn!");
@@ -351,11 +360,12 @@ var yGuess;
           $("#winner").text("Player" + " " + game.whoseTurn().mark + " WINS!");
         }
 
-
         game.changeTurn();
+
         if (game.whoseTurn().mark === "O") {
 
-        compuTurn();
+         compuTurn();
+         alert("o turn");
         }
 
         $("#players h1").text("Player" + " " + game.whoseTurn().mark + "'s Turn!");
@@ -379,12 +389,12 @@ var yGuess;
           $("#winner").text("Player" + " " + game.whoseTurn().mark + " WINS!");
         }
 
-
-
         game.changeTurn();
+
         if (game.whoseTurn().mark === "O") {
 
-        compuTurn();
+          compuTurn();
+          alert("o turn");
         }
 
         $("#players h1").text("Player" + " " + game.whoseTurn().mark + "'s Turn!");
@@ -397,41 +407,40 @@ var yGuess;
     });
 
     var compuTurn = function() {
-      var fake;
-      var xGuess = Math.floor(Math.random() * 3);
-      var yGuess = Math.floor(Math.random() * 3);
-
-
+    var fake;
+    var xGuess = Math.floor(Math.random() * 3);
+    var yGuess = Math.floor(Math.random() * 3);
 
     if(xGuess === 0 && yGuess === 0) {
     fake = "#top-left";
     }
-    if(xGuess === 1 && yGuess === 0) {
-    fake = "#mid-left";
-    }
-    if(xGuess === 2 && yGuess === 0) {
-    fake = "#bottom-left";
-    }
     if(xGuess === 0 && yGuess === 1) {
     fake = "#top-mid";
-    }
-    if(xGuess === 1 && yGuess === 1) {
-    fake = "#mid-mid";
-    }
-    if(xGuess === 2 && yGuess === 1) {
-    fake = "#bottom-mid";
     }
     if(xGuess === 0 && yGuess === 2) {
     fake = "#top-right";
     }
+    if(xGuess === 1 && yGuess === 0) {
+    fake = "#mid-left";
+    }
+    if(xGuess === 1 && yGuess === 1) {
+    fake = "#mid-mid";
+    }
     if(xGuess === 1 && yGuess === 2) {
     fake = "#mid-right";
+    }
+    if(xGuess === 2 && yGuess === 0) {
+    fake = "#bottom-left";
+    }
+    if(xGuess === 2 && yGuess === 1) {
+    fake = "#bottom-mid";
     }
     if(xGuess === 2 && yGuess === 2) {
     fake = "#bottom-right";
     }
 
     if ((!(board.isMarked(xGuess, yGuess))) && (game.winner() === false)) {
+
       game.board.mark(xGuess, yGuess, game.whoseTurn().mark);
       $(fake).text(game.whoseTurn().mark);
       if ((game.winner()) === "Draw") {
@@ -441,21 +450,21 @@ var yGuess;
         $("#results").slideDown("slow");
         $("#winner").text("Player" + " " + game.whoseTurn().mark + " WINS!");
       }
-    game.changeTurn();
-    $("#players h1").text("Player" + " " + game.whoseTurn().mark + "'s Turn!");
-    $("body").removeClass();
-    $("body").toggleClass(game.whoseTurn().mark);
-    if (game.winner()) {
-      $("#players").hide();
+
+      game.changeTurn();
+      $("#players h1").text("Player" + " " + game.whoseTurn().mark + "'s Turn!");
+      $("body").removeClass();
+      $("body").toggleClass(game.whoseTurn().mark);
+      if (game.winner()) {
+        $("#players").hide();
 
     }
     }
+    else {
+      compuTurn();
 
-    };
-
-
-
-
+    }
+  };
 
     $("button#reset").click(function() {
       location.reload();
